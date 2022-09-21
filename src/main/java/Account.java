6e731b358a2a06ab0190d6a1ca3d85fb5e90cad8
omarmoustafa11 <omaroj99@yahoo.com>
2022-09-21 18:42:55 -0400

    hw

---
+++ src/main/java/Account.java
@@ -0,0 +1,15 @@
+public class Account {
+
+    private int balance;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        if( balance >= 0){
+        this.balance = balance;
+
+    }}
+
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+       Account account1 = new Account();
+       account1.setBalance(-10);
+        System.out.println(account1.getBalance());
+
+    }
+}
